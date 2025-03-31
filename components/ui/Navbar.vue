a80c9d00585fd81cac5199fcb9f0592d49b9262c
ccadran <clar.cadran@gmail.com>
2025-03-31 17:08:41 +0200

    feat(nav): create navbar

---
+++ components/ui/Navbar.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup></script>
+
+<template>
+  <nav class="navbar">
+    <a class="home" href="/">TEKTURE</a>
+    <ul class="nav-links">
+      <li class="nav-link menu-item">Works</li>
+      <li class="nav-link menu-item">About</li>
+      <li class="nav-link menu-item">Contact</li>
+    </ul>
+  </nav>
+</template>
+
+<style lang="scss">
+.navbar {
+  position: fixed;
+  top: 14px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: calc(100vw - 60px);
+  left: 50%;
+  transform: translateX(-50%);
+
+  > .home {
+    font-size: 18px;
+    font-weight: 800;
+  }
+  > .nav-links {
+    display: flex;
+    > .nav-link {
+      margin-left: 32px;
+    }
+  }
+}
+</style>
