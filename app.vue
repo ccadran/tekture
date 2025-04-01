d7b5fb493bd585bc7d1a22fc9449ed9f30d51e3c
ccadran <clar.cadran@gmail.com>
2025-04-01 17:13:19 +0200

    fix(hero anim): kill image anim before start an other

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
     <UiNavbar />
-    <Hero />
+    <HomeHero />
   </div>
 </template>
---
+++ components/home/Hero.vue
@@ -0,0 +1,132 @@
+<script lang="ts" setup>
+import gsap from 'gsap'
+import { onMounted, onUnmounted, ref } from 'vue'
+
+const images = ref([
+  { id: 1, src: '/images/1.png' },
+  { id: 2, src: '/images/2.png' },
+  { id: 3, src: '/images/3.png' },
+  { id: 4, src: '/images/4.png' },
+  { id: 5, src: '/images/1.png' },
+  { id: 6, src: '/images/2.png' },
+  { id: 7, src: '/images/3.png' },
+])
+
+const imageRefs = ref<HTMLImageElement[]>([])
+const DISTANCE_X_THRESHOLD = 350
+const DISTANCE_Y_THRESHOLD = 100
+
+const mouseTracking = ref({
+  currentX: 0,
+  currentY: 0,
+  previousX: null as number | null,
+  previousY: 0,
+  distanceX: 0,
+  distanceY: 0,
+  totalXdistance: 0,
+  totalYdistance: 0,
+  currentImageIndex: 0,
+})
+
+const handleMouseMove = (e: MouseEvent) => {
+  const m = mouseTracking.value
+  m.currentX = e.clientX
+  m.currentY = e.clientY
+  if (m.previousX !== null) {
+    m.distanceX = Math.abs(m.currentX - m.previousX)
+    m.distanceY = Math.abs(m.currentY - m.previousY)
+
+    m.totalXdistance += m.distanceX
+    m.totalYdistance += m.distanceY
+
+    if (m.totalXdistance > DISTANCE_X_THRESHOLD || m.totalYdistance > DISTANCE_Y_THRESHOLD) {
+      animateImage(m.currentImageIndex, e.clientX, e.clientY)
+      m.totalXdistance = 0
+      m.totalYdistance = 0
+
+      m.currentImageIndex = (m.currentImageIndex + 1) % imageRefs.value.length
+    }
+  }
+  m.previousX = m.currentX
+  m.previousY = m.currentY
+}
+
+const animateImage = (imageIndex: number, x: number, y: number) => {
+  const image = imageRefs.value[imageIndex]
+  if (!image) return
+  gsap.killTweensOf(image)
+
+  const imagesTl = gsap.timeline()
+  imagesTl.fromTo(image, { x, y: y + 50, opacity: 0 }, { y, opacity: 1 }).to(image, { y: y + 50, opacity: 0 }, 0.5)
+}
+onMounted(() => {
+  window.addEventListener('mousemove', handleMouseMove)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('mousemove', handleMouseMove)
+})
+</script>
+
+<template>
+  <div class="mousemove-images">
+    <div
+      v-for="(image, index) in images"
+      :key="image.id"
+      class="floating-image"
+      :ref="
+(el) => {
+  if (el) imageRefs[index] = el as HTMLImageElement
+}
+"
+    >
+      <img :src="image.src" :alt="`Image ${image.id}`" />
+    </div>
+  </div>
+  <section class="hero">
+    <div class="content">
+      <h1>TEKTURE</h1>
+      <div class="baseline">
+        <p>new gen architectural studio</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss">
+.hero {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  > .content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 32px;
+    > h1 {
+      font-size: 124px;
+      font-weight: 800;
+    }
+  }
+}
+.mousemove-images {
+  height: 100vh;
+  width: 100vw;
+  position: absolute;
+  pointer-events: none;
+  > .floating-image {
+    position: absolute;
+    opacity: 0.5;
+    width: 200px;
+    aspect-ratio: 3/4;
+    overflow: hidden;
+    > img {
+      object-fit: cover;
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
+</style>
