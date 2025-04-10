1eab4e6cccc8ff8a22f988a91a864e987175f75f
ccadran <clar.cadran@gmail.com>
2025-04-10 17:06:44 +0200

    feat(manifest): start anim

--- components/home/Manifest.vue
+++ components/home/Manifest.vue
@@ -1,9 +1,25 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import gsap from 'gsap'
+const manifestContent1 = ref<HTMLElement | null>(null)
+const manifestContent2 = ref<HTMLElement | null>(null)
+let sentences1
+let sentences2
+
+onMounted(() => {
+  sentences1 = manifestContent1.value!.querySelectorAll('.sentence .inner')
+  sentences2 = manifestContent2.value!.querySelectorAll('.sentence .inner')
+
+  console.log(sentences1)
+
+  const manifestTl = gsap.timeline()
+  manifestTl.set(sentences2, { y: 20 }).to(sentences1, { y: -20, stagger: 0.2 }).to(sentences2, { y: 0, stagger: 0.2 }, 0)
+})
+</script>
 
 <template>
   <section class="manifest">
     <div class="manifest-container">
-      <div class="manifest-content">
+      <div class="manifest-content" ref="manifestContent1">
         <div class="sentence">
           <p class="inner">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni asperiores molestiae obcaecati at quasi ex!</p>
         </div>
@@ -14,7 +30,7 @@
           <p class="inner">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni asperiores molestiae obcaecati at quasi ex!</p>
         </div>
       </div>
-      <div class="manifest-content">
+      <div class="manifest-content" ref="manifestContent2">
         <div class="sentence">
           <p class="inner">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni asperiores molestiae obcaecati at quasi ex!</p>
         </div>
@@ -45,6 +61,10 @@
       position: absolute;
       display: flex;
       flex-direction: column;
+      gap: 24px;
+      > .sentence {
+        overflow: hidden;
+      }
     }
   }
 }
