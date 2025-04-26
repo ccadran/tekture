885592d1d96887bf78891987e159c76bc8f3cf45
ccadran <clar.cadran@gmail.com>
2025-04-26 19:19:06 +0200

    feat(projects): good layout & project switch animations

--- components/home/Projects.vue
+++ components/home/Projects.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import gsap from 'gsap'
+import SplitType from 'split-type'
 
 const projects = [
   {
@@ -9,76 +10,92 @@ const projects = [
   },
   {
     name: 'CENTRE POMPIDOU',
-    description: 'Qui fugiat qui amet excepteur occaecat in esse eiusmod officia ullamco ad aliqua ex.',
+    description: '2Qui fugiat qui amet excepteur occaecat in esse eiusmod officia ullamco ad aliqua ex.',
     images: ['/images/3.png', '/images/4.png'],
   },
   {
     name: '23 DESIGN',
-    description: 'Qui fugiat qui amet excepteur occaecat in esse eiusmod officia ullamco ad aliqua ex.',
+    description: '3Qui fugiat qui amet excepteur occaecat in esse eiusmod officia ullamco ad aliqua ex.',
     images: ['/images/2.png', '/images/3.png'],
   },
   {
     name: 'ELARCH',
-    description: 'Qui fugiat qui amet excepteur occaecat in esse eiusmod officia ullamco ad aliqua ex.',
+    description: '4Qui fugiat qui amet excepteur occaecat in esse eiusmod officia ullamco ad aliqua ex.',
     images: ['/images/3.png', '/images/1.png'],
   },
   {
     name: 'STUSSY STORE',
-    description: 'Qui fugiat qui amet excepteur occaecat in esse eiusmod officia ullamco ad aliqua ex.',
+    description: '5Qui fugiat qui amet excepteur occaecat in esse eiusmod officia ullamco ad aliqua ex.',
     images: ['/images/4.png', '/images/2.png'],
   },
 ]
 
 const activeProjectIndex = ref<number>(0)
 
-const projectText = ref<HTMLElement | null>(null)
+/**
+ * ANIMATE ELEMENT
+ */
+const titleChars = ref<NodeListOf<Element> | null>(null)
+const descriptionLines = ref<NodeListOf<Element> | null>(null)
 const leftImage = ref<HTMLElement | null>(null)
 const rightImage = ref<HTMLElement | null>(null)
 
-const projectOutTl = ref<gsap.core.Timeline>()
-const projectInTl = ref<gsap.core.Timeline>()
-
 onMounted(() => {
-  projectOutTl.value = gsap
-    .timeline({ paused: true, onComplete: () => {} })
-    .to(projectText.value, { opacity: 0, duration: 0.5, ease: 'power1.out' })
-    .to(leftImage.value, { opacity: 0, duration: 0.5, ease: 'power1.out' }, 0)
-    .to(rightImage.value, { opacity: 0, duration: 0.5, ease: 'power1.out' }, 0)
-
-  projectInTl.value = gsap
-    .timeline({ paused: true })
-    .to(projectText.value, { opacity: 1, duration: 0.5, ease: 'power1.out' })
-    .to(leftImage.value, { opacity: 1, duration: 0.5, ease: 'power1.out' }, 0)
-    .to(rightImage.value, { opacity: 1, duration: 0.5, ease: 'power1.out' }, 0)
+  new SplitType('.description ', { types: 'lines' })
+  new SplitType('.title ', { types: 'chars' })
+  wrapLinesWithInner()
 })
+
+const projectOut = (index: number) => {
+  const targetProject = document.querySelector(`.project--${index} `)
+  gsap
+    .timeline()
+    .to(targetProject, { opacity: 0, duration: 0.5, ease: 'power1.out' })
+    .set(targetProject, { zIndex: projects.length - 1 })
+}
+const projectIn = (index: number) => {
+  const targetProject = document.querySelector(`.project--${index} `) as HTMLElement
+  titleChars.value = targetProject?.querySelectorAll('.title .char')
+  descriptionLines.value = targetProject?.querySelectorAll('.description .line .inner')
+  leftImage.value = targetProject?.querySelector('.left-images .project-image')
+  rightImage.value = targetProject?.querySelector('.right-images .project-image')
+  console.log(leftImage.value)
+
+  gsap
+    .timeline()
+    .set(targetProject, { zIndex: projects.length, opacity: 1 })
+    .fromTo(titleChars.value, { opacity: 0, y: 100 }, { opacity: 1, y: 0, stagger: { each: 0.025, from: 'random' }, duration: 0.5, ease: 'power1.inOut' })
+    .fromTo(
+      descriptionLines.value,
+      { opacity: 0, y: 100 },
+      { opacity: 1, y: 0, stagger: { each: 0.025, from: 'random' }, duration: 0.5, ease: 'power1.inOut' },
+      0.2
+    )
+    .fromTo(leftImage.value, { opacity: 0 }, { opacity: 1, duration: 0.5, ease: 'power1.inOut' }, 0.3)
+    .fromTo(rightImage.value, { opacity: 0 }, { opacity: 1, duration: 0.5, ease: 'power1.inOut' }, 0.35)
+}
+
+const wrapLinesWithInner = () => {
+  const lineElements = document.querySelectorAll('.line')
+
+  lineElements.forEach((line) => {
+    const content = line.innerHTML
+
+    line.innerHTML = `<div class="inner">${content}</div>`
+  })
+}
 
 const changeProject = (index: number) => {
   if (index === activeProjectIndex.value) return
-
-  resetTimelines()
-  setTimeout(() => {
-    activeProjectIndex.value = index
-    projectInTl.value!.play()
-  }, 500)
-}
-
-const resetTimelines = () => {
-  projectOutTl.value!.progress(0)
-  projectInTl.value!.progress(0)
-
-  projectOutTl.value!.pause()
-  projectInTl.value!.pause()
-  projectOutTl.value!.play()
+  projectOut(activeProjectIndex.value)
+  projectIn(index)
+  activeProjectIndex.value = index
 }
 </script>
 
 <template>
   <section class="projects">
     <div class="project-layout">
-      <div class="project-text" ref="projectText">
-        <h2>{{ projects[activeProjectIndex].name }}</h2>
-        <p>{{ projects[activeProjectIndex].description }}</p>
-      </div>
       <nav class="projects-navigation">
         <div class="markers"></div>
         <ul>
@@ -87,15 +104,22 @@ const resetTimelines = () => {
           </li>
         </ul>
       </nav>
+      <div :class="'project-content project--' + index" v-for="(project, index) in projects">
+        <div class="project-text">
+          <h2 class="title">{{ project.name }}</h2>
 
-      <div class="left-images" ref="leftImage">
-        <div class="project-image" :class="activeProjectIndex % 2 === 0 ? 'top' : 'bottom'">
-          <img :src="projects[activeProjectIndex].images[0]" alt="" />
+          <p class="description">{{ project.description }}</p>
         </div>
-      </div>
-      <div class="right-images" ref="rightImage">
-        <div class="project-image" :class="activeProjectIndex % 2 === 0 ? 'bottom' : 'top'">
-          <img :src="projects[activeProjectIndex].images[1]" alt="" />
+
+        <div class="left-images">
+          <div class="project-image" :class="index % 2 === 0 ? 'top' : 'bottom'">
+            <img :src="project.images[0]" alt="" />
+          </div>
+        </div>
+        <div class="right-images">
+          <div class="project-image" :class="index % 2 === 0 ? 'bottom' : 'top'">
+            <img :src="project.images[1]" alt="" />
+          </div>
         </div>
       </div>
     </div>
@@ -105,22 +129,14 @@ const resetTimelines = () => {
 <style lang="scss">
 .project-layout {
   height: 100vh;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
   position: relative;
-  > .project-text {
-    display: flex;
-    flex-direction: column;
-    gap: 8px;
-    text-align: center;
-    max-width: 600px;
-    margin-top: 14vh;
-  }
+
   > .projects-navigation {
     position: absolute;
+    z-index: 10;
+    left: 50%;
     top: 50%;
-    transform: translateY(-50%);
+    transform: translate(-50%, -50%);
     > ul {
       display: flex;
       flex-direction: column;
@@ -128,42 +144,83 @@ const resetTimelines = () => {
       text-align: center;
     }
   }
-  .project-image {
+  > .project-content {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
     position: absolute;
-    > img {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
+    height: 100%;
+    width: 100%;
+    // visibility: hidden;
+    opacity: 0;
+    &.project--0 {
+      opacity: 1;
+      z-index: 5;
+      // visibility: visible;
     }
-  }
-  > .left-images {
-    .top {
-      left: 72px;
-      top: 10vh;
-      width: 254px;
-      aspect-ratio: 254/317;
+    > .project-text {
+      display: flex;
+      flex-direction: column;
+      gap: 8px;
+      text-align: center;
+      align-items: center;
+      max-width: 600px;
+      margin-top: 14vh;
+      > .title {
+        width: fit-content;
+        height: fit-content;
+        overflow: hidden;
+        > .char {
+        }
+      }
+      > .description {
+        > .line {
+          height: fit-content;
+          width: fit-content;
+          overflow: hidden;
+          > .inner {
+          }
+        }
+      }
     }
-    .bottom {
-      left: 72px;
-      bottom: 10vh;
-      width: 394px;
-      aspect-ratio: 394/277;
-    }
-  }
-  > .right-images {
-    .top {
+
+    .project-image {
       position: absolute;
-      right: 72px;
-      top: 10vh;
-      width: 254px;
-      aspect-ratio: 254/317;
+      > img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
     }
-    .bottom {
-      position: absolute;
-      right: 72px;
-      bottom: 10vh;
-      width: 394px;
-      aspect-ratio: 394/277;
+    > .left-images {
+      .top {
+        left: 72px;
+        top: 10vh;
+        width: 254px;
+        aspect-ratio: 254/317;
+      }
+      .bottom {
+        left: 72px;
+        bottom: 10vh;
+        width: 394px;
+        aspect-ratio: 394/277;
+      }
+    }
+    > .right-images {
+      .top {
+        position: absolute;
+        right: 72px;
+        top: 10vh;
+        width: 254px;
+        aspect-ratio: 254/317;
+      }
+      .bottom {
+        position: absolute;
+        right: 72px;
+        bottom: 10vh;
+        width: 394px;
+        aspect-ratio: 394/277;
+      }
     }
   }
 }
