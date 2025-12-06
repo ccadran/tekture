b824159dc499b86024cbc21783cf9fbbd95216dc
ccadran <clar.cadran@gmail.com>
2025-12-06 16:58:02 +0100

    feat(all projects): move each project to top after each other based on scroll

--- pages/all-projects.vue
+++ pages/all-projects.vue
@@ -1,10 +1,19 @@
 <script lang="ts" setup>
 import projectsData from '~/assets/data/projects.json'
 import ScrollTrigger from 'gsap/ScrollTrigger'
-
+import gsap from 'gsap'
 const stickyContainer = ref<HTMLElement | null>(null)
+const projectsContainer = ref<HTMLElement | null>(null)
+const projectDataRefs = ref<HTMLElement[]>([])
+const projectAssetsRefs = ref<HTMLElement[]>([])
+const projectNumber = ref<number>(0)
+const targetProject = ref<number>(0)
+const scrollTriggerInstance = ref<ScrollTrigger | null>(null)
 
 onMounted(() => {
+  projectNumber.value = projectsData.length
+  stickyContainer.value!.style.height = window.innerHeight * (projectNumber.value + 1) + 'px'
+
   const scrollTrigger = ScrollTrigger.create({
     trigger: stickyContainer.value,
     start: 'top top',
@@ -13,14 +22,85 @@ onMounted(() => {
       console.log(self)
     },
   })
+  const arrayTest = []
+  const sectionHeight = window.innerHeight
+  console.log(sectionHeight, stickyContainer.value?.clientHeight)
+  console.log(projectNumber.value)
+
+  for (let i = 0; i < projectNumber.value; i++) {
+    const start = 0 + sectionHeight * i
+    const end = 0 + sectionHeight * (i + 1)
+    const object = {
+      start,
+      end,
+    }
+    arrayTest.push(object)
+  }
+  console.log(arrayTest)
+  //After entry anim
+  projectDataRefs.value.forEach((projectData) => {
+    projectData.style.top = projectData.getBoundingClientRect().top + 'px'
+    projectData.style.position = 'absolute'
+    projectData.style.width = '100%'
+  })
+
+  console.log(projectDataRefs.value[0].getBoundingClientRect().top)
+  changeProjectOnScroll()
+
+  arrayTest.forEach((step, index) => {
+    gsap.to(projectDataRefs.value[index], {
+      top: 0,
+      scrollTrigger: {
+        trigger: stickyContainer.value,
+        start: step.start,
+        end: step.end,
+        scrub: true,
+      },
+    })
+  })
 })
+
+function changeProjectOnScroll() {
+  let lastStep = -1
+  let step = 1 / (projectNumber.value + 1)
+
+  scrollTriggerInstance.value = ScrollTrigger.create({
+    trigger: stickyContainer.value,
+    start: 'top top',
+    end: 'bottom bottom',
+    onUpdate: (self) => {
+      const rawStep = Math.floor(self.progress / step)
+      const currentProjectIndex = Math.min(rawStep, projectNumber.value)
+
+      // gsap.to(projectDataRefs.value[0], {
+      //   transform: 'translateY(100%)',
+      //   scrollTrigger: {
+      //     start: `${stickyContainer.value?.clientHeight}`,
+      //   },
+      // })
+      console.log(currentProjectIndex)
+
+      if (currentProjectIndex !== lastStep) {
+        lastStep = currentProjectIndex
+      }
+    },
+  })
+}
 </script>
 
 <template>
   <div class="main" ref="stickyContainer">
-    <div class="project-container">
+    <div class="projects-container" ref="projectsContainer">
       <div class="projects-list">
-        <div class="project-data" v-for="project in projectsData">
+        <div
+          class="project-data"
+          v-for="(project, index) in projectsData"
+          :ref="
+            (el) => {
+              if (el) projectDataRefs[index] = el as HTMLElement
+            }
+          "
+        >
           <div class="location">
             <p>{{ project.location }}</p>
           </div>
@@ -36,7 +116,15 @@ onMounted(() => {
         </div>
       </div>
       <div class="projects-assets">
-        <div class="project-asset" v-for="project in projectsData">
+        <div
+          class="project-asset"
+          v-for="(project, index) in projectsData"
+          :ref="
+            (el) => {
+              if (el) projectAssetsRefs[index] = el as HTMLElement
+            }
+          "
+        >
           <div class="l-image"><img :src="project.images[1]" alt="" /></div>
           <div class="main-image"><img :src="project.images[0]" alt="" /></div>
           <div class="r-image"><img :src="project.images[2]" alt="" /></div>
@@ -49,7 +137,7 @@ onMounted(() => {
 <style lang="scss">
 .main {
   height: 600vh;
-  .project-container {
+  .projects-container {
     background-color: white;
     position: sticky;
     height: 100svh;
@@ -58,8 +146,10 @@ onMounted(() => {
       position: fixed;
       bottom: 0;
       width: 100%;
+      height: 100%;
       display: flex;
       flex-direction: column;
+      justify-content: end;
       gap: 4px;
 
       .project-data {
@@ -96,6 +186,7 @@ onMounted(() => {
       width: 100vw;
       position: fixed;
       top: 0;
+      display: none;
       > .project-asset {
         position: absolute;
         height: 100%;
